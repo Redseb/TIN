25bf00294b0a3d1ff36190017026b9f7173aa373
Redseb <mikolajzyzanski@gmail.com>
2021-10-05 14:26:34 +0200

    Working on lab 8

--- lab8/lab8.js
+++ lab8/lab8.js
@@ -1,1 +1,16 @@
-const express =require('express');
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+app.get('/hello', (req, res) => {
+    res.send('Hello World!')
+});
+
+app.get('/form', (req, res) => {
+    res.sendFile(path.join(__dirname, '/form.html'));
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
