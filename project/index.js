facfd3419db918cf5972ce95dfdd4edb802c8d0f
Redseb <mikolajzyzanski@gmail.com>
2021-10-18 12:14:32 +0200

    test

--- project/index.js
+++ project/index.js
@@ -91,5 +91,5 @@ app.post("/highscore", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Retro Bird running on http://localhost:${port}`);
 });
